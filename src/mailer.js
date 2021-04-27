467ef90b1c800e3e9d178a000ec69eb76284045f
Rodrigo M <r_miranda_11@yahoo.com>
2021-04-27 18:26:21 -0500

    update

--- src/mailer.js
+++ src/mailer.js
@@ -77,26 +77,26 @@ const app = express()
 // });
 // app.get('/', handler);
 
-app.use(cors())
+// app.use(cors())
+app.use(cors({origin: '*'}));
+// app.use(function (req, res, next) {
 
-app.use(function (req, res, next) {
+//   // Website you wish to allow to connect
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080/send');
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080/send');
+//   // Request methods you wish to allow
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   // Request headers you wish to allow
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
 
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader('Access-Control-Allow-Credentials', false);
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', false);
-
-  // Pass to next layer of middleware
-  next();
-});
+//   // Pass to next layer of middleware
+//   next();
+// });
 // app.use(cors({origin: 'http://localhost:8080/send'}));
 app.use(express.json())
 app.use('/', router)
