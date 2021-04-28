5735e690c468c7b2bb403d29ebc28554c572c743
Rodrigo M <r_miranda_11@yahoo.com>
2021-04-28 14:24:19 -0500

    upate

--- src/mailer.js
+++ src/mailer.js
@@ -72,14 +72,35 @@ router.post('/send', (req, res, next) => {
 
 
 const app = express()
-// var corsOptions = {
-//   origin: 'http://localhost:8080/send',
-//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }
+app.all('*', function(req, res,next) {
+  /**
+   * Response settings
+   * @type {Object}
+   */
+  var responseSettings = {
+      "AccessControlAllowOrigin": req.headers.origin,
+      "AccessControlAllowHeaders": "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name",
+      "AccessControlAllowMethods": "POST, GET, PUT, DELETE, OPTIONS",
+      "AccessControlAllowCredentials": true
+  };
 
-// app.use('/', cors(corsOptions), function (req, res, next) {
-//   res.json({msg: 'This is CORS-enabled for only example.com.'})
-// })
+  /**
+   * Headers
+   */
+  res.header("Access-Control-Allow-Credentials", responseSettings.AccessControlAllowCredentials);
+  res.header("Access-Control-Allow-Origin",  responseSettings.AccessControlAllowOrigin);
+  res.header("Access-Control-Allow-Headers", (req.headers['access-control-request-headers']) ? req.headers['access-control-request-headers'] : "x-requested-with");
+  res.header("Access-Control-Allow-Methods", (req.headers['access-control-request-method']) ? req.headers['access-control-request-method'] : responseSettings.AccessControlAllowMethods);
+
+  if ('OPTIONS' == req.method) {
+      res.send(200);
+  }
+  else {
+      next();
+  }
+
+
+});
 
 app.use(express.json())
 app.use('/', router)
