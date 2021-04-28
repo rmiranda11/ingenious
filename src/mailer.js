1b77dd827236ccd565e8441994c98223012f6095
Rodrigo M <r_miranda_11@yahoo.com>
2021-04-28 15:33:39 -0500

    update

--- src/mailer.js
+++ src/mailer.js
@@ -72,38 +72,9 @@ router.post('/send', (req, res, next) => {
 
 
 const app = express()
-app.all('*', function(req, res,next) {
-  /**
-   * Response settings
-   * @type {Object}
-   */
-  var responseSettings = {
-      "AccessControlAllowOrigin": req.headers.origin,
-      "AccessControlAllowHeaders": "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name",
-      "AccessControlAllowMethods": "POST, GET, PUT, DELETE, OPTIONS",
-      "AccessControlAllowCredentials": true
-  };
-
-  /**
-   * Headers
-   */
-  res.header("Access-Control-Allow-Credentials", responseSettings.AccessControlAllowCredentials);
-  res.header("Access-Control-Allow-Origin",  responseSettings.AccessControlAllowOrigin);
-  res.header("Access-Control-Allow-Headers", (req.headers['access-control-request-headers']) ? req.headers['access-control-request-headers'] : "x-requested-with");
-  res.header("Access-Control-Allow-Methods", (req.headers['access-control-request-method']) ? req.headers['access-control-request-method'] : responseSettings.AccessControlAllowMethods);
-
-  if ('OPTIONS' == req.method) {
-      res.send(200);
-  }
-  else {
-      next();
-  }
-
-
-});
 
 app.use(express.json())
-app.use('/', router)
+app.use('/send', router)
 app.use(cors());
 app.listen(8080)
 
