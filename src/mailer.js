51545c8997917e7fb5fd9a3bfbffdfdfe2fc8a5f
Rodrigo M <r_miranda_11@yahoo.com>
2021-04-30 21:59:30 -0500

    update

--- src/mailer.js
+++ src/mailer.js
@@ -75,14 +75,20 @@ router.post('/send', cors(), (req, res, next) => {
   })
 })
 
-const app = expre
-ss()
+const app = express()
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://ingeniouscarpentry.com"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
+// const cors = require('cors');
+const corsOptions ={
+    origin:'http://ingeniouscarpentry.com', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 app.use(cors())
 app.use(express.json())
