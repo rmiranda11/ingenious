e4f4b34291e4f10ed7ba5dccda793ab5c329d8f1
Rodrigo M <r_miranda_11@yahoo.com>
2021-05-19 16:54:25 -0500

    update

--- src/mailer.js
+++ src/mailer.js
@@ -36,7 +36,7 @@ transporter.verify((error, success) => {
 //   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 // }
 
-router.post('/send', (req, res, next) => {
+router.post('/send', cors(corsOptions), (req, res, next) => {
   var name = req.body.name
   var phone = req.body.phone
   var email = req.body.email
@@ -85,10 +85,12 @@ const app = express()
 // const cors = require('cors');
 const corsOptions ={
     origin:'http://ingeniouscarpentry.com/', 
+    methods:"POST",
+    allowedHeaders:"Content-Type",
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 // app.use(cors())
 app.use(express.json())
