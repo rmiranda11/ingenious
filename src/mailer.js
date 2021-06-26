82ba4d4ea56eb8f61b0be9004483fd29096e0634
Rodrigo M <r_miranda_11@yahoo.com>
2021-06-25 21:34:10 -0500

    cors

--- src/mailer.js
+++ src/mailer.js
@@ -36,7 +36,7 @@ transporter.verify((error, success) => {
 //   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 // }
 
-router.post('/send', cors(corsOptions), (req, res, next) => {
+router.post('/send', cors(), (req, res, next) => {
   var name = req.body.name
   var phone = req.body.phone
   var email = req.body.email
@@ -76,23 +76,14 @@ router.post('/send', cors(corsOptions), (req, res, next) => {
 })
 
 const app = express()
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://ingeniouscarpentry.com"); // update to match the domain you will make the request from
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+     next();
+});
 
-// const cors = require('cors');
-const corsOptions ={
-    origin:'http://ingeniouscarpentry.com/', 
-    methods:"POST, GET, OPTIONS, DELETE" ,
-    allowedHeaders:"Accept,Origin,Content-Type,X-LS-CORS-Template,X-LS-Auth-Token,X-LS-Auth-User-Token,Content-Type,X-LS-Sync-Result,X-LS-Sequence,token",
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-// app.use(cors(corsOptions));
-
-// app.use(cors())
+app.use(cors())
 app.use(express.json())
 app.use('/', router)
 app.listen(8080)
